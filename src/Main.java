41132b61bdbd34e16016296dd7130528f839f68a
Tomas Krejcik <dracek95@gmail.com>
2020-05-20 10:41:40 +0200

    Updated Main.java
    Uploaded wrong version of Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,39 @@
 
 public class Main
 {
+	public static void PNumber(int n)
+	{
 
-	public static boolean PrimeNumber(int num) {
-		if(num/num == 1)
-			return true;
-		else
-			return false;
+		int tempInt = 0;
+		int isItZero = 0;
+
+		tempInt = n / 2;
+
+		for (int i = 2; i <= tempInt; i++)
+		{
+
+			if (n % i == 0)
+			{
+				System.out.println(n + " is not prime number");
+				isItZero = 1;
+				break;
+			}
+			if (n == 0 || n == 1)
+			{
+				System.out.println(n + " is not prime number");
+				break;
+			}
+		}
+		if (isItZero == 0)
+		{
+			System.out.println(n + " is a prime number");
+		}
+
 	}
-	
-	
+
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		PNumber(13);
 
 	}
 
