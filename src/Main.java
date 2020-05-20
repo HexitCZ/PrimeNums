ad1dd852f569312563d78d9c1a4ca93fba69cc83
Tomas Krejcik <dracek95@gmail.com>
2020-05-20 10:32:29 +0200

    Uploaded .java files

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main
+{
+
+	public static boolean PrimeNumber(int num) {
+		if(num/num == 1)
+			return true;
+		else
+			return false;
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
